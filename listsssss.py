cda13019d1be16ab9b6c2789929fbc12f4c64182
vivian19-meet <vivian19@meet.mit.edu>
2017-07-30 18:18:57 +0300

    Stopping here

---
+++ listsssss.py
@@ -0,0 +1,44 @@
+Python 3.5.2 (default, Nov 17 2016, 17:05:23) 
+[GCC 5.4.0 20160609] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> a= ['she','sea','shells','by','the','sea','shore']
+>>> b="selfish selfish"
+>>> c=[1,1,2,3,5,8,13]
+>>> b[3:4]
+'f'
+>>> c[6:7]
+[13]
+>>> c[5:6]
+[8]
+>>> a[4:7]
+['the', 'sea', 'shore']
+>>> a[4]
+'the'
+>>> a[3]
+'by'
+>>> a[2]
+'shells'
+>>> a[2:4]
+['shells', 'by']
+>>> a[2-1]
+'sea'
+>>> b = "selfish shellfish"
+>>> b[9:14]
+'hellf'
+>>> b[10:14]
+'ellf'
+>>> b[8:14]
+'shellf'
+>>> a*3
+['she', 'sea', 'shells', 'by', 'the', 'sea', 'shore', 'she', 'sea', 'shells', 'by', 'the', 'sea', 'shore', 'she', 'sea', 'shells', 'by', 'the', 'sea', 'shore']
+>>> dog='dalmatian'
+>>> len(dog)
+9
+>>> dogs=[dog,'poodle','boxer']
+>>> len(dogs)
+3
+>>> def string_test(s):
+	if len(s)>=2
+	
+SyntaxError: invalid syntax
+>>> 
